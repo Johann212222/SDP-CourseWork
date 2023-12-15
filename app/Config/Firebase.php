d9d31b837f468052a098f9358a19c8223b95afd2
Ayazmax <134224527+Ayazmax@users.noreply.github.com>
2023-12-15 17:22:40 +0530

    done connecting

---
+++ app/Config/Firebase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Config;
+
+class Firebase
+{
+    public $credentialsPath = APPPATH . 'config\firebaseKey\dss-sdp-firebase-adminsdk-ic39t-97eca82608.json';
+    public $databaseUrl = 'https://dss-sdp-default-rtdb.firebaseio.com'; // Your Firebase Realtime Database URL
+}
---
+++ app/Models/FirebaseModel.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+class FirebaseModel
+{
+    private $database;
+
+    public function __construct()
+    {
+        $serviceAccount = ServiceAccount::fromValue(config('Firebase')->credentialsPath);
+
+        $firebase = (new Factory())
+            ->withServiceAccount($serviceAccount)
+            ->withDatabaseUri(config('Firebase')->databaseUrl);
+
+        $this->database = $firebase->createDatabase();
+    }
+
+    public function getData($path)
+    {
+        $reference = $this->database->getReference($path);
+        $snapshot = $reference->getSnapshot();
+
+        return $snapshot->getValue();
+    }
+
+    // Add other methods as needed
+}
